2851becd83db8bac6a08a74edaaef8b106e9c423
snake <lcpdfk@163.com>
2020-09-13 15:24:21 +0800

    78. 子集

---
+++ Letter_Combinations_of_a_Phone_Number/solution.cpp
@@ -0,0 +1,23 @@
+#include "solution.h"
+#include<iostream>
+
+vector<vector<int>> Solution::subsets(vector<int>& nums) {
+    //路径数组
+    //记录走过的路径，相当于实际上遍历解空间树的路径
+    //因为从根节点出发最后需要回到根节点，因此执行完毕后，该数组将为空
+    vector<int> track;
+    backtrack(0, nums, track);
+    return res;
+}
+void Solution::backtrack(int start, vector<int>& nums, vector<int>& track) {
+    res.push_back(track);
+    for (unsigned int i = start; i < nums.size(); i++) {
+        //修改变量
+        track.push_back(nums[i]);
+        //传递至下一层
+        backtrack(i + 1, nums, track);
+        //还原变量
+        track.pop_back();
+        //cout << endl;
+    }
+}
--- Subsets/solution.h
+++ Subsets/solution.h
@@ -8,6 +8,6 @@ private:
 	vector<vector<int>> res;
 public:
 	vector<vector<int>> subsets(vector<int>& nums);
-	void backtrack(int start, vector<int>& nums, vector<int> track);
+	void backtrack(int start, vector<int>& nums, vector<int>& track);
 };
 
